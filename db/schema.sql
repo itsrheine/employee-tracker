bd907995f8451d5847551a45ee5ec9d7eb17d558
Marrione Nguyen <mtiamsic@gmail.com>
2020-10-08 17:01:40 -0700

    initial commit

---
+++ db/schema.sql
@@ -0,0 +1,18 @@
+DROP TABLE IF EXISTS employee;
+
+CREATE TABLE employee (
+  id INTEGER PRIMARY KEY,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+);
+
+CREATE TABLE roles (
+  id INTEGER PRIMARY KEY,
+  title VARCHAR(30) NOT NULL,
+  salary DECIMAL NOT NULL
+);
+
+CREATE TABLE department (
+  id INTEGER PRIMARY KEY,
+  name VARCHAR(30) NOT NULL
+);
