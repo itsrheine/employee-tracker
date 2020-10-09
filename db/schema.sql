a30356d0eb5907f1c77038aa599a92e4d8208e34
Marrione Nguyen <mtiamsic@gmail.com>
2020-10-09 02:10:21 -0700

    added schema departments and role

--- db/schema.sql
+++ db/schema.sql
@@ -1,18 +1,26 @@
 DROP TABLE IF EXISTS employee;
 
-CREATE TABLE employee (
-  id INTEGER PRIMARY KEY,
+CREATE TABLE employees (
+  id INTEGER AUTO_INCREMENT,
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
+  role_id INT NOT NULL,
+  PRIMARY KEY (id),
+  CONSTRAINT fk_department FOREIGN KEY (role_id) REFERENCES roles(id),
+  manager_id INT UNSIGNED
 );
 
 CREATE TABLE roles (
   id INTEGER PRIMARY KEY,
   title VARCHAR(30) NOT NULL,
-  salary DECIMAL NOT NULL
+  salary DECIMAL NOT NULL,
+  department_id INT NOT NULL,
+  PRIMARY KEY (id),
+  INDEX dep_ind (department_id),
+  CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES departments(id)
 );
 
-CREATE TABLE department (
+CREATE TABLE departments (
   id INTEGER PRIMARY KEY,
   name VARCHAR(30) NOT NULL
 );
