49c217940da89ddb96db65e252e30d58f79a73e2
Marrione Nguyen <mtiamsic@gmail.com>
2020-10-09 20:52:10 -0700

    updated server

---
+++ server.js
@@ -0,0 +1,134 @@
+const inquirer = require('inquirer');
+
+
+const trackerTitle = () => {
+
+    console.log (
+        `
+        +------------------+
+        | EMPLOYEE TRACKER |
+        +------------------+
+        `
+    )
+};
+
+// Main index node directory
+const employeeTracker = () => {
+
+    inquirer.prompt([
+        {
+            type: 'checkbox',
+            name: 'index',
+            message: 'What would you like to do?',
+            choices: [
+                'View all employees',
+                'View all employees by department',
+                'View all employees by manager',
+                'Add an employee',
+                'Remove an employee',
+                'Update employee role',
+                'Update employee manager'
+            ]
+        }
+    ])
+        .then(function(value) {
+            switch (value.index) {
+
+                case 'View all employees':
+                    viewAllEmployees();
+                    break;
+
+                case 'View all employees by department':
+                    viewAllByDept();
+                    break;
+
+                case 'View all employees by manager':
+                    viewAllByMgr();
+                    break;
+
+                case 'Add an employee':
+                    addEmployee();
+                    break;
+
+                case 'Remove an employee':
+                    removeEmployee();
+                    break;
+
+                case 'Update employee role':
+                    updateEmpRole();
+                    break;
+
+                case 'Update employee manager':
+                    updateEmpMgr();
+                    break;
+            }
+        })
+};
+
+function viewAllEmployees() {
+    // console log the table of all employees
+    employeeTracker();
+};
+
+function viewAllByDept() {
+    // console log the table of all employees
+    employeeTracker();
+};
+
+function viewAllByMgr() {
+    // console log the table of all employees
+    employeeTracker();  
+};
+
+function addEmployee() {
+    inquirer.prompt([
+        {
+            type: 'text',
+            name: 'firstname',
+            message: `What is the employee's first name?`
+        },
+        {
+            type: 'text',
+            name: 'lastname',
+            message: `What is the employee's last name?`
+        },
+        {
+            type: 'checkbox',
+            name: 'role',
+            message: `What is the employee's role?`,
+            choices: [
+                'Sales Lead',
+                'Salesperson',
+                'Lead Engineer',
+                'Software Engineer',
+                'Accountant',
+                'Legal Team Lead',
+                'Lawyer'
+            ]
+        },
+        {
+            type: 'confirm',
+            name: 'mid',
+            message: `Are you a manager?`
+        },
+        {
+            type: 'checkbox',
+            name: 'manager',
+            message: `Who is the employee's manager?`,
+            choices: [
+                'Sales Lead',
+                'Salesperson',
+                'Lead Engineer',
+                'Software Engineer',
+                'Accountant',
+                'Legal Team Lead',
+                'Lawyer'
+            ]
+        },
+    ])
+};
+
+
+// initialize node
+trackerTitle();
+employeeTracker();
---
+++ utils/inputCheck.js
@@ -0,0 +1,1 @@
+const { timeStamp } = require("console");
